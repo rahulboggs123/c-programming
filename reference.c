d2ddad2bc7057acfdef08592759f610f89b5dd5d
rahulboggs123 <49330953+rahulboggs123@users.noreply.github.com>
2019-04-06 12:27:49 +0530

    Update reference.c

--- reference.c
+++ reference.c
@@ -4,21 +4,20 @@ void get_input(int *a,int *b)
        printf("enter 2 nos");
        scanf("%d%d",a,b);
 }
-int  add2nos(int  a,int b)
+int  add2nos(int a,int b,int *c)
 {
-    int c;
-    c=a+b;
-    return c;
+    *c=a+b;
+    return *c;
 }
-void output(int a,int b,int *c)
+void output(int a,int b,int c)
 {
-     printf("%d+%d=%d",a,b,*c);
+     printf("%d+%d=%d",a,b,c);
 }
 int main()
     {
         int a,b,c;
         get_input(&a,&b);
-        c=add2nos(&a,&b);
-        output(a,b,&c);
+        c=add2nos(a,b,&c);
+        output(a,b,c);
         return 0;
     }
