ef2ea28c0ccea921a469d0fe3fe7e2f185b0702f
rahulboggs123 <49330953+rahulboggs123@users.noreply.github.com>
2019-04-06 15:23:02 +0530

    Update sum of n nos.c

--- sum of n nos.c
+++ sum of n nos.c
@@ -5,36 +5,38 @@ void input1(int *n)
     printf("enter the number of elements");
     scanf("%d",n);
 }
-void  input2(int a[n])
+void  input2(int n,int a[n])
 {
-    for(i=0;i<ni++)
+    for(i=0;i<n;i++)
      {
+        printf("enter the next number\n");
         scanf("%d",&a[i]);
      }
 }
 int compute( int n,int a[n])
 { 
+    int sum=0;
    for (i=0;i<n;i++)
      {
         sum=sum+a[i];
      }
      return sum; 
 }
-void output(int sum ,int a[n])
+void output(int n ,int sum ,int a[n])
 {
-  printf("the sum of");
-  for(i=0;i<ni++)
+  printf("the sum of\t");
+  for(i=0;i<n;i++)
   {
-      printf("%d\t +"i);
+      printf("%d +",a[i]);
   }
-  printf("=%d"sum);
+  printf("=%d",sum);
 }
 int main()
 {
     int n,a[100],sum;
     input1(&n) ;
-    input2(a[n]); 
-    sum=compute(n,a[n]);
-    output();
+    input2(n,&a[n]); 
+    sum=compute(n,&a[n]);
+    output(n,sum,&a[n]);
     return 0;
 }
