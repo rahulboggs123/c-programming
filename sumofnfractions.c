11a88f159fecdae9cb87e02ace4b25797343f4dc
rahulboggs123 <49330953+rahulboggs123@users.noreply.github.com>
2019-04-07 15:13:19 +0530

    Update sumofnfractions.c

--- sumofnfractions.c
+++ sumofnfractions.c
@@ -2,9 +2,9 @@
 void input(int *n)
 {
     printf("enter the number of fractions");
-    scanf("%d",n)
+    scanf("%d",n);
 }
-void input1(int n ,int num[n],int den[n] )
+void input1(int n ,int num[n],int den[n])
 {
     int i;
     printf("enter all the numerators");
@@ -23,35 +23,39 @@ int compute(int n,int den[n])
     }
     return lcm;
 }
-int  compute1(int n, int num[n],int den[n]],x)
+int  compute1(int n, int num[n],int den[n],int x)
+{   
+    int i,factor,sum=0;
+    for(i=0;i<n;i++)
+    {
+        //if(den[i]==0)
+           // return den[i];
+        if(den[i]!=0)
+          {
+            factor=x/den[i] ;
+            sum=sum+(factor*num[i]);
+          }
+      
+    }
+   return sum;
+}
+void output(int n,int num[n],int den[n],int x,int s)
 {   
     int i;
-    for(i=0,i<n;i++)
-    {
-        if(den[i]==0)
-        return den[i];
-    }
-    
-    for(i=0,i<n;i++)
-    {
-        if(den[i]!=0)
-        {
 
-        }
-        
+    for(i=0;i<n;i++)
+    {
+        printf("%d/%d+",num[i],den[i]);
     }
-   
-}
-void output()
-{   
-     
+    printf("=%d/%d",s,x);
 }
 void main()
 {
-   int n,num[],den[],x;
+   int n,num[100],den[100],x,s;
    input(&n);
    input1(n,&num[n],&den[n]) ;
-   x=compute(n,&den[n])
-   compute1(n,&num[n],&den[n],x);
-
+   x=compute(n,&den[n]);
+   s=compute1(n,&num[n],&den[n],x);
+   output( n,&num[n],&den[n],x,s);
+   
 }
