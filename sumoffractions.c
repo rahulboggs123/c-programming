a5637fac69817f2aea6e267d8006a449d231d793
rahulboggs123 <49330953+rahulboggs123@users.noreply.github.com>
2019-04-07 11:23:12 +0530

    Update sumoffractions.c

--- sumoffractions.c
+++ sumoffractions.c
@@ -8,53 +8,40 @@ void input(int*num1,int*den1,int*num2,int*den2)
 }
 int compute1(int num1,int den1,int num2,int den2)
 {
-    int num3;
+    int num3,den3;float result;
     if(den1==den2==0)
-       return 1;
+       return 0;
     if(den1==1)
         {
            num3=(den2*num1)+num2;
+           den3=den2;
         } 
     else if(den2==1) 
         {
             num3=(den1*num2)+num1;
+            den3=den1;
         } 
     else
         {
             num3=(den1*num2)+(den2*num1);
+            den3=den2*den1;
         }
-        return num3;
+        result=num3/den3;
+        return result;
 }
-int  compute2(int den1,int den2)
-{    
-    int den3;
-    if(den1==1)
-       {
-           den3=den2;
-       }
-    else if(den2==1)
-       {
-           den3=den1;
-       }
-    else
-       {
-           den3=den1*den2;
-       }
-       return den3;
-}
-void output(int num1,int den1,int num2,int den2,int x,int y)
+void output(int num1,int den1,int num2,int den2,int x)
 {   
-     if(x==1)
+     if(x==0)
         printf("invalid input");
      printf("%d %d %d\n",num1,num2,x) ;
      printf("_+_=_\n");
-     printf("%d %d %d\n",den1,den2,y) ;
+     printf("%d %d %d\n",den1,den2) ;
 }
 void main()
 {
     int num1,den1,num2,den2,x,y;
     input(&num1,&den1,&num2,&den2);
     x=compute1(num1,den1,num2,den2);
-    y=compute2(den1,den2);
-    output(num1,den1,num2,den2,x,y);
+    output(num1,den1,num2,den2,x);
 }
+
