903939d195fdfaf009e9b999668f9bfe0b26e847
rahulboggs123 <49330953+rahulboggs123@users.noreply.github.com>
2019-04-07 11:48:42 +0530

    Update sumoffractions.c

--- sumoffractions.c
+++ sumoffractions.c
@@ -6,42 +6,41 @@ void input(int*num1,int*den1,int*num2,int*den2)
     printf("enter numerator and denominator of second fraction");
     scanf("%d%d",num2,den2);
 }
-int compute1(int num1,int den1,int num2,int den2)
+int  compute1(int num1,int den1,int num2,int den2,int *den3)
 {
-    int num3,den3;float result;
+    int num3;
     if(den1==den2==0)
-       return 0;
+       return 100000;
     if(den1==1)
         {
            num3=(den2*num1)+num2;
-           den3=den2;
+           *den3=den2;
         } 
     else if(den2==1) 
         {
             num3=(den1*num2)+num1;
-            den3=den1;
+            *den3=den1;
         } 
     else
         {
             num3=(den1*num2)+(den2*num1);
-            den3=den2*den1;
+            *den3=den2*den1;
         }
-        result=num3/den3;
-        return result;
+        return num3;
 }
-void output(int num1,int den1,int num2,int den2,int x)
+void output(int num1,int den1,int num2,int den2,int x,int*den3)
 {   
-     if(x==0)
+     if(x==100000)
         printf("invalid input");
      printf("%d %d %d\n",num1,num2,x) ;
      printf("_+_=_\n");
-     printf("%d %d %d\n",den1,den2) ;
+     printf("%d %d %d \n",den1,den2,*den3) ;
 }
 void main()
 {
-    int num1,den1,num2,den2,x,y;
+    int num1,den1,num2,den2,den3,x,y;
     input(&num1,&den1,&num2,&den2);
-    x=compute1(num1,den1,num2,den2);
-    output(num1,den1,num2,den2,x);
-}
+    x=compute1(num1,den1,num2,den2,&den3);
+    output(num1,den1,num2,den2,x,&den3);
 
+}
